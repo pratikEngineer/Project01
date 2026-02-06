a2b23f9d3075ddeee918c60e5558a48d0421879c
pratikEngineer <pratikkokate633@gmail.com>
2026-02-07 00:24:09 +0530

    updates count program

---
+++ src/main/scala/sparkfunctionalities/word_count.scala
@@ -0,0 +1,38 @@
+package sparkfunctionalities
+
+object word_count {
+  import org.apache.spark.{SparkConf, SparkContext}
+
+    def main(args: Array[String]): Unit = {
+
+      val conf = new SparkConf()
+        .setAppName("WordCount")
+        .setMaster("local[*]")
+
+      val sc = new SparkContext(conf)
+
+      // Input data
+      val data = List(
+        "hello spark",
+        "hello scala",
+        "spark with scala",
+        "hello spark"
+      )
+
+      // Create RDD
+      val rdd = sc.parallelize(data)
+
+      // Word count logic
+      val wordCount = rdd
+        .flatMap(line => line.split(" "))
+        .map(word => (word, 1))
+        .reduceByKey(_ + _)
+
+      // Print result
+      wordCount.collect().foreach(println)
+
+      sc.stop()
+    }
+
+
+}
