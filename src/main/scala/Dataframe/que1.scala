e45bcc15992bc958ce42802c0fea9cdda4e10c6e
pratikEngineer <pratikkokate633@gmail.com>
2026-02-18 17:49:25 +0530

    comments #  just to try

--- src/main/scala/Dataframe/que1.scala
+++ src/main/scala/Dataframe/que1.scala
@@ -1,31 +1,32 @@
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
+//import org.apache.spark.sql.SparkSession
+//import org.apache.spark.sql.functions._
+//
+//object SparkConditionalColumns {
+//  def main(args: Array[String]): Unit = {
+//
+//    // Create Spark Session
+//    val spark = SparkSession.builder()
+//      .appName("Conditional Columns Examples")
+//      .master("local[4]")
+//      .getOrCreate()
+//
+//    import spark.implicits._
+//
+//    println("=" * 60)
+//    println("SIMPLE QUESTIONS")
+//    println("=" * 60)
+//
+//    // Question 1: Conditional Column
+//    println("\n1. Conditional Column (is_adult)")
+//    val employees = List(
+//      (1, "AJAY", 28),
+//      (2, "VIJAY", 35),
+//      (3, "MANOJ", 22)
+//    ).toDF("id", "name", "age")
+//
+//    val employees1 = employees.withColumn("is_adult", col("age") >= 18)
+//    employees1.show()
+//
+//  }
+//}
 
-object SparkConditionalColumns {
-  def main(args: Array[String]): Unit = {
-
-    // Create Spark Session
-    val spark = SparkSession.builder()
-      .appName("Conditional Columns Examples")
-      .master("local[4]")
-      .getOrCreate()
-
-    import spark.implicits._
-
-    println("=" * 60)
-    println("SIMPLE QUESTIONS")
-    println("=" * 60)
-
-    // Question 1: Conditional Column
-    println("\n1. Conditional Column (is_adult)")
-    val employees = List(
-      (1, "AJAY", 28),
-      (2, "VIJAY", 35),
-      (3, "MANOJ", 22)
-    ).toDF("id", "name", "age")
-
-    val employees1 = employees.withColumn("is_adult", col("age") >= 18)
-    employees1.show()
-
-  }
-}
