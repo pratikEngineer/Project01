15b9f9611ec88aa1e6683399a6e38e4b339fab62
pratikEngineer <pratikkokate633@gmail.com>
2026-02-22 21:30:35 +0530

    different way

--- src/main/scala/Dataframe/que1.scala
+++ src/main/scala/Dataframe/que1.scala
@@ -1,23 +1,17 @@
-//import org.apache.spark.sql.SparkSession
-//import org.apache.spark.sql.functions._
-//
-//object SparkConditionalColumns {
-//  def main(args: Array[String]): Unit = {
-//
-//    // Create Spark Session
-//    val spark = SparkSession.builder()
-//      .appName("Conditional Columns Examples")
-//      .master("local[4]")
-//      .getOrCreate()
-//
-//    import spark.implicits._
-//
-//    println("=" * 60)
-//    println("SIMPLE QUESTIONS")
-//    println("=" * 60)
-//
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object SparkConditionalColumns {
+  def main(args: Array[String]): Unit = {
+
+    // Create Spark Session
+    val spark = SparkSession.builder()
+      .appName("Conditional Columns Examples")
+      .master("local[4]")
+      .getOrCreate()
+
 //    // Question 1: Conditional Column
-//    println("\n1. Conditional Column (is_adult)")
+//
 //    val employees = List(
 //      (1, "AJAY", 28),
 //      (2, "VIJAY", 35),
@@ -26,7 +20,16 @@
 //
 //    val employees1 = employees.withColumn("is_adult", col("age") >= 18)
 //    employees1.show()
-//
-//  }
-//}
+
+
+    val employees2 = List(
+      (1, "Ajay", 28),
+      (2, "Vijay", 35),
+      (3, "Manoj", 22)
+    ).toDF("id", "name", "age")
+    val result = employees2.withColumn("is_adult", when(col("age") > 18, True).otherwise(False))
+        result.show()
+
+  }
+}
 
